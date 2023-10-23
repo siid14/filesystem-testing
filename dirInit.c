1cdeeaac5e4df6e53347a98d36cd5316e5c316ce
RuxueJ <124758534+RuxueJ@users.noreply.github.com>
2023-10-22 20:20:13 -0700

    Update dirInit.c

--- dirInit.c
+++ dirInit.c
@@ -1,8 +1,8 @@
 /**************************************************************
 * Class:  CSC-415-01  Fall 2023
-* Names: 
-* Student IDs: 
-* GitHub Name:
+* Names: Sidney Thomas, Hoang-Anh Tran, Ruxue Jin, Yee-Tsing Yang
+* Student IDs: 918656419, 922617784, 923092817, 922359864
+* GitHub Name: siid14, htran31, RuxueJ, Y-Y1Q
 * Group Name: Alibaba
 * Project: Basic File System
 *
