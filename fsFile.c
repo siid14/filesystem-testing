e07c54c74bd9bbb0bc8ccf5c4ba74d6c2dab491b
Y-Y1Q <sh4bee@outlook.com>
2023-11-05 23:30:21 -0800

    Add fsFile

---
+++ fsFile.c
@@ -0,0 +1,19 @@
+/**************************************************************
+ * Class:  CSC-415-01 Fall 2023
+ * Names: Sidney Thomas, Hoang-Anh Tran, Ruxue Jin, Yee-Tsing Yang
+ * Student IDs: 918656419, 922617784, 923092817, 922359864
+ * GitHub Name: siid14, htran31, RuxueJ, Y-Y1Q
+ * Group Name: Alibaba
+ * Project: Basic File System
+ *
+ * File: fsFile.c
+ *
+ * Description:
+ *
+ *
+ *
+ **************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fsFile.h"
