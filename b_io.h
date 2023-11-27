7cd9e5697fb385fc8f213ee26b52f80510550bf7
Y-Y1Q <143047725+Y-Y1Q@users.noreply.github.com>
2023-11-27 15:46:27 -0800

    Update b_io.h

--- b_io.h
+++ b_io.h
@@ -1,5 +1,5 @@
 /**************************************************************
- * Class:  CSC-415-01  Fall 2021
+ * Class:  CSC-415-01  Fall 2023
  * Names: Sidney Thomas, Hoang-Anh Tran, Ruxue Jin, Yee-Tsing Yang
  * Student IDs: 918656419, 922617784, 923092817, 922359864
  * GitHub Name: siid14, htran31, RuxueJ, Y-Y1Q
